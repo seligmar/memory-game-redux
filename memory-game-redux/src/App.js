7535723986e39ee40c401c0c309ae8cad2227bbf
github username <github email address>
2019-12-24 15:58:55 -0500

    removed all login/end game code

--- memory-game-redux/src/App.js
+++ memory-game-redux/src/App.js
@@ -3,25 +3,14 @@ import './App.css'
 import './index.css'
 import paintings from './data/paintings'
 import BoardGame from './BoardGame'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 import { withRouter } from 'react-router-dom'
-
-const SAVEGAMEURL = 'http://localhost:3000/save-game'
-
-const MySwal = withReactContent(Swal)
-
-let timeElapsed = 0
 
 class App extends React.Component {
   state = {
     paintings: [],
     indeciesToPlay: [],
-    runtime: 0, // set state at end of game and then do patch request
     showStartGameButton: true
   }
-
-  timerHandle = null
 
   setNewArrayofPaintings = () => {
     const index = [...Array(97).keys()] // this works
@@ -69,67 +58,13 @@ class App extends React.Component {
   }
 
   startGame = () => {
-    this.startTimer()
     this.createNewArray()
     this.setState({ showLeaderboard: true })
   }
 
-  startTimer = () => {
-    this.setState({ runtime: 0, timerOn: true })
-    this.timerHandle = setInterval(() => {
-      timeElapsed += 1
-      this.setState({ runtime: timeElapsed })
-    }, 1000)
-  }
-
-  endTimer = () => {
-    clearInterval(this.timerHandle)
-    MySwal.fire({
-      imageUrl: 'https://media.giphy.com/media/h5AHEcNMhn7u8/giphy.gif',
-      imageWidth: 300,
-      imageHeight: 200,
-      imageAlt: 'Bob Ross',
-      animation: false,
-      confirmButtonText: 'Im done',
-      background: '#090526',
-      padding: '.25em',
-      confirmButtonColor: 'cornflowerblue',
-      width: 300
-    })
-    this.setState({ indeciesToPlay: [] })
-  }
-
-  postData = (username, score) => {
-    const Newuser = {
-      user: {
-        username: username,
-        highScore: score
-      }
-    }
-    return fetch(SAVEGAMEURL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(Newuser)
-    })
-      .then(resp => resp.json())
-      .then(resp => console.log(resp))
-  }
-
   endGame = () => {
     this.setState({ showStartGameButton: false })
-    this.endTimer()
-    this.postData(this.props.currentPlayer, this.state.runtime)
-    this.setState({ showLeaderboard: false })
     //  this.props.history.push('/')
-  }
-
-  quitApp = () => {
-    if (this.timerHandle === null) {
-      this.props.history.push('/')
-    } else {
-      clearInterval(this.timerHandle)
-      this.props.history.push('/')
-    }
   }
 
   render () {
@@ -149,16 +84,6 @@ class App extends React.Component {
         ) : (
           <h2>CONGRATS, THANKS FOR PLAYING :)</h2>
         )}
-        <br />
-        <button
-          size='large'
-          primary
-          className='start-page-buttons'
-          onClick={() => this.quitApp()}
-        >
-          {'   '}
-          Logout {'  '}
-        </button>
         <br />
         <BoardGame
           endGame={this.endGame}
